aa2cdf1073b7d369e19d309cb179bf8d4b5d9f62
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2023-12-27 07:44:23 +0100

    Create currency.js

---
+++ src/components/currency.js
@@ -0,0 +1,38 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+  const [currency, setCurrency] = useState('');
+  const { dispatch } = useContext(AppContext);
+
+  const handleChange = (event) => {
+    const selectedCurrency = event.target.value;
+    setCurrency(selectedCurrency);
+    changeCurrency(selectedCurrency); // Pass the updated value directly
+  };
+
+  const changeCurrency = (selectedCurrency) => {
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: selectedCurrency,
+    });
+  };
+
+  useEffect(() => {
+    console.log(currency);
+  }, [currency]);
+
+  return (
+    <div className="alert alert-secondary">
+      <select id="inputGroupSelect02" onChange={handleChange}>
+        <option defaultValue>Currency</option>
+        <option value="$" name="dollar">$ Dollar</option>
+        <option value="£" name="pound">£ Pound</option>
+        <option value="€" name="euro">€ Euro</option>
+        <option value="₹" name="ruppee">₹ Ruppee</option>
+      </select>
+    </div>
+  );
+};
+
+export default Budget;
